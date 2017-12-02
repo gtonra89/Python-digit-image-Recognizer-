9a0df799a4fd0bc15bf53d081e9d103932c2d83b
Garret Tonra <gtonra89@gmail.com>
2017-12-02 15:56:09 +0000

    added the model and its layers next to fit the model

--- model/train.py
+++ model/train.py
@@ -69,5 +69,38 @@ testY = keras.utils.to_categorical(testY, NumberOfClasses)
 #We can then add layers to our model using the add() method.
 model = Sequential()
 
+# We add Conv2D layer. Allows spatial convolution over images. 
+#This is a 2d convolution layer.
+# It is the first layer in the model and it create a convolution kernal.
+# All parameters are added to the layer
+model.add(Conv2D(32, # 32 represents the amount of filters for your conv2d layer.
+                kernel_size=(3, 3),# Kernal_size receives a list/tuple of 2 integers specifying width and height of a 2D convolution window. 
+                activation='relu',# relu is a activations function. Activation functions allows us to get better accuracy. Depending on which activation function you use, it can provide lower or higher gradients. Relu allows the network the max amount of the error in back propagation.
+                shape=shape))# Input shape represents the elements an array has in each dimension(ie. image_rows,image_cols,1)
+
+# Add another convolution layer with parameters (filters,kernal_size and activation function)
+model.add(Conv2D(64, (3, 3), activation='relu'))
+
+# The MaxPooling2D layer is used for downsampling the image size.
+#a (2,2) pool it splits a pixel image into 4 chucks
+# and takes the 4 highest values from each chunk to represent or (2,2) pool
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# A Dropout method consists in randomly setting a fraction rate of input units to 0 
+# at each update during training time, which helps prevent overfitting.
+model.add(Dropout(0.25))
+# it flattens the input provided
+model.add(Flatten())
+# Add another a dense layer with a output of 128 (nodes) & relu yet again
+model.add(Dense(128, activation='relu'))
+# A Dropout method consists in randomly setting a fraction rate of input units to 0 at each update during training time, which helps prevent overfitting.
+model.add(Dropout(0.5))
+# Apply a final dense layer with a output of num_class(10) which is our number of outcomes and use activation function softmax. We apply the softmax activation function to the final layer as it can be used to represent catagorical data. Outputing results ranging from 0 upto 1
+model.add(Dense(NumberOfClasses, activation='softmax'))
+# Compile method configures our model before training. Its takes arguments: Optimizer, loss and metrics
+model.compile(loss=keras.losses.categorical_crossentropy,# Loss(objective) Function. Categorical_crossentrophy: catagorises our loss with our catagories
+              optimizer=keras.optimizers.Adadelta(),# Adadelta Optimizer used to leave perametersat their default values. Better algorithm then the classic gradient descent
+              metrics=['accuracy'])# Used to judge the performance of your model
+# To train our model we use the fit function
+
 
 
