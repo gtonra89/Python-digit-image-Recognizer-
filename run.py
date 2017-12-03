7027c4516583c731e100f3273ea14e65f4a3460b
Garret Tonra <gtonra89@gmail.com>
2017-12-03 16:53:03 +0000

    added about page done a bit of styling

--- run.py
+++ run.py
@@ -15,6 +15,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+	
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 # Analayse the input given function
 # we use get and post actions
@@ -22,10 +26,11 @@ def index():
 def analyse():
 # calls the parse img function to 
 # get data from drawing canvas and save as image
-    parseImg(request.get_data())
+    parseImage(request.get_data())
 
     # read parsed image back in 8-bit, black and white mode (L)
-    parseRead = imread('output.png', mode='L') 
+    parseRead = imread('output.png', mode='L')
+	#Computes the bit-wise NOT of the underlying binary representation of the integers in the input arrays
     parseRead = np.invert(parseRead)
     parseRead = imresize(parseRead,(28,28))
 
@@ -33,19 +38,23 @@ def analyse():
     parseRead = parseRead.reshape(1,28,28,1)
 
     # load model to predict number
+	#You can then use keras.models.load_model(filepath) to reinstantiate your model.
+	#load_model will also take care of compiling the model using the saved training configuration 
+	#(unless the model was never compiled in the first place).
     trainedModel = keras.models.load_model("model/mnistModel.h5")
 
     # Use predict function and pass image parseRead through it to get answer
     Result = trainedModel.predict(parseRead)
     print(Result)
 	
-	 # change Result to number string
+	# change Result to number string
+	#Return a string representation of the data in the array.
     ResString = np.array_str(np.argmax(Result, axis=1))
     print(ResString)
     return ResString
 	
 # Parsing Image function
-def parseImg(imgData):
+def parseImage(imgData):
     # parse canvas bytes and save as output.png
     imgstr = re.search(b'base64,(.*)', imgData).group(1)
     with open('output.png','wb') as output:
